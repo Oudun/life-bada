6a7225fea2a72494e84ce42395c28306db86809c
ddreval <ddreval@localhost>
2014-06-07 14:19:00 +0400

    removing build dir

--- .Target-Debug/.gdbinit
+++
@@ -1,4 +0,0 @@
-cd D:/bada/2.0.3/IDE/workspace/Life/.Target-Debug/
-add-symbol-file Life.exe 0x693aa000
-cd D:/bada/2.0.3/IDE/workspace/Life/.Target-Debug/
-core coredmp
--- .Target-Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := D:/bada/2.0.3/Model/WaveWQVGA/Target/osp_rt0.a D:/bada/2.0.3/Model/WaveWQVGA/Target/FOsp.so D:/bada/2.0.3/Model/WaveWQVGA/Target/StubDynCast.so
-
-LIBS :=
